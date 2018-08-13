fd36e03a48a61bd56f3abf4ac2e399466aa745e1
Derek Alia <derekalia@me.com>
2018-08-13 00:22:08 -0700

    added old draft

--- docs/getting-started.md
+++ docs/getting-started.md
@@ -5,6 +5,7 @@ title: Getting started
 
 Aragon is a project to empower freedom by making decentralized governance happen.
 We have built multiple pieces of infrastructure to make this happen:
+
 - **aragonOS**: A framework that enables flexible and upgradeable governance mechanisms by creating and assigning permissions to multiple entities.
 - **aragon.js**: A Javascript library to interact with aragonOS by handling transaction pathing, upgradeability and state of the contracts.
 - **Aragon UI**: An Aragon-native toolkit of UI components for decentralized apps.
@@ -12,3 +13,57 @@ We have built multiple pieces of infrastructure to make this happen:
 
 Although these components are very powerful together, you can use them indivually.
 We will explore how to do both.
+
+=-=-=-=-=-=-=-
+
+What you need to get started building with Aragon.
+
+- [Goals of Aragon](#goals-of-aragon)
+- [Environment setup](#environment-setup)
+- [Quick start](#quick-start)
+- [Next](#next)
+
+## Goals of Aragon
+
+**What are DAOs?**
+
+DAOs allow us to have organizations that live on the blockchain, allowing for features such as censorship resistance, decentralization and transparency. If you want to know more about DAOs checkout [link](https://en.wikipedia.org/wiki/Decentralized_autonomous_organization) and [link](https://blog.ethereum.org/2014/05/06/daos-dacs-das-and-more-an-incomplete-terminology-guide/).
+
+**What can we do with them?**
+
+Aragon DAOs are super easy to create. All you have to do is enter a few details and pay the gas fee. Once you have a DAO you can begin inviting members to start participating within your organization including but not limited to: creating governance models, voting on issues, setting up payroll and polling the community.
+Lets jump into building our first DAO
+
+##Environment setup
+
+**Node version**
+
+Make sure you have at least Node >= 8.0.
+
+**Metamask**
+
+We recommend using the Metamask browser extentsion(https://metamask.io/) to send transaction to the blockchain.
+
+**Install the Aragon CLI**
+
+From the command line run:
+
+```bash
+npm i -g @aragon/cli
+```
+
+Once we have this package installed we can start building DAOs from the command line.
+
+**Quick start**
+
+To create your first DAO run:
+
+```bash
+aragon init foo.aragonpm.eth
+```
+
+Congrats you have just created a DAO! It’s running on your local network and you can check it out by going to localhost:3001
+
+**Next**
+
+Now that you’ve built a DAO let's take a look at the docs to learn the docs or you can jump into the tutorials.
--- website/siteConfig.js
+++ website/siteConfig.js
@@ -1,122 +1,119 @@
-const React = require('react')
-const { BaseStyles, PublicUrl } = require('@aragon/ui')
-const ServerStyleSheet = require('styled-components').ServerStyleSheet
-const renderToStaticMarkup = require('react-dom/server').renderToStaticMarkup
+const React = require("react");
+const { BaseStyles, PublicUrl } = require("@aragon/ui");
+const ServerStyleSheet = require("styled-components").ServerStyleSheet;
+const renderToStaticMarkup = require("react-dom/server").renderToStaticMarkup;
 
 /* List of projects/orgs using your project for the users page */
 const users = [
   {
-    caption: 'district0x',
-    image: '/img/users/district0x.svg',
-    infoLink: 'https://district0x.io',
-    pinned: true,
+    caption: "district0x",
+    image: "/img/users/district0x.svg",
+    infoLink: "https://district0x.io",
+    pinned: true
   },
   {
-    caption: 'Decentraland',
-    image: '/img/users/decentraland.png',
-    infoLink: 'https://decentraland.org',
-    pinned: true,
+    caption: "Decentraland",
+    image: "/img/users/decentraland.png",
+    infoLink: "https://decentraland.org",
+    pinned: true
   },
   {
-    caption: 'Althea',
-    image: '/img/users/althea.jpg',
-    infoLink: 'https://altheamesh.com',
-    pinned: true,
-  },
-]
+    caption: "Althea",
+    image: "/img/users/althea.jpg",
+    infoLink: "https://altheamesh.com",
+    pinned: true
+  }
+];
 
 const siteConfig = {
-  title: 'Aragon Developer Portal',
-  tagline: 'What you need to get started building with Aragon',
-  url: 'https://hack.aragon.org',
-  baseUrl: '/',
-  cname: 'hack.aragon.org',
+  title: "Aragon Developer Portal",
+  tagline: "What you need to get started building with Aragon",
+  url: "https://hack.aragon.org",
+  baseUrl: "/",
+  cname: "hack.aragon.org",
   // For github.io type URLs, you would set the url and baseUrl like:
   //   url: 'https://facebook.github.io',
   //   baseUrl: '/test-site/',
 
-  projectName: 'hack-docs',
-  organizationName: 'aragon',
+  projectName: "hack-docs",
+  organizationName: "aragon",
 
   headerLinks: [
-    {doc: 'getting-started', label: 'Get started'},
-    {doc: 'tutorial', label: 'Tutorial'},
-    {doc: 'aragonos-ref', label: 'aragonOS'},
-    {doc: 'kernel_Kernel', label: 'API'},
-    {doc: 'aragonjs-ref', label: 'aragon.js'},
-    {doc: 'aragonui-intro', label: 'Aragon UI'},
-    {page: 'help', label: 'Help'},
-    {blog: true, label: 'Blog'},
+    { doc: "getting-started", label: "Get started" },
+    { doc: "tutorial", label: "Tutorial" },
+    { doc: "aragonos-ref", label: "aragonOS" },
+    { doc: "kernel_Kernel", label: "API" },
+    { doc: "aragonjs-ref", label: "aragon.js" },
+    { doc: "aragonui-intro", label: "Aragon UI" },
+    { href: "https://github.com/aragon/hack", label: "GitHub" }
   ],
 
   // If you have users set above, you add it here:
   users,
 
   /* path to images for header/footer */
-  headerIcon: 'img/isotype.svg',
-  footerIcon: 'img/isotype.svg',
-  favicon: 'img/favicon.png',
+  headerIcon: "img/isotype.svg",
+  footerIcon: "img/isotype.svg",
+  favicon: "img/favicon.png",
 
   /* colors for website */
   // #00c8e4
   colors: {
-    primaryColor: '#3b3b3b',
-    secondaryColor: '#00b4e6',
+    primaryColor: "#000000",
+    secondaryColor: "#ffffff"
   },
 
   /* custom fonts for website */
   fonts: {
-    myFont: [
-      "Overpass",
-    ],
-    myOtherFont: [
-      "-apple-system",
-      "system-ui"
-    ]
+    myFont: ["Overpass"],
+    myOtherFont: ["-apple-system", "system-ui"]
   },
 
+  //highlighting for jsx
+  usePrism: ["jsx"],
+
   // This copyright info is used in /core/Footer.js and blog rss/atom feeds.
-  copyright:
-    'Creative Commons ' +
-    new Date().getFullYear() +
-    ' Aragon Project',
+  copyright: "Creative Commons " + new Date().getFullYear() + " Aragon Project",
 
   highlight: {
     // Highlight.js theme to use for syntax highlighting in code blocks
-    theme: 'default',
+    theme: "atom-one-dark"
   },
 
+  // Add custom scripts here that would be placed in <script> tags
+  scripts: ["https://buttons.github.io/buttons.js"],
+
   /* On page navigation for the current documentation page */
-  onPageNav: 'separate',
+  onPageNav: "separate",
 
   /* Open Graph and Twitter card images */
-  ogImage: 'img/docusaurus.png',
-  twitterImage: 'img/docusaurus.png',
+  ogImage: "img/docusaurus.png",
+  twitterImage: "img/docusaurus.png",
 
   // You may provide arbitrary config keys to be used as needed by your
   // template. For example, if you need your repo's URL...
   //   repoUrl: 'https://github.com/facebook/test-site',
 
   renderToString: element => {
-    const sheet = new ServerStyleSheet()
+    const sheet = new ServerStyleSheet();
     const html = renderToStaticMarkup(
       sheet.collectStyles(
         React.createElement(
           PublicUrl.Provider,
-          { url: '/aragon-ui/' },
+          { url: "/aragon-ui/" },
           React.createElement(BaseStyles, { enableLegacyFonts: true }),
           element
         )
       )
-    )
-    const insertStylesAt = html.lastIndexOf('</body>')
+    );
+    const insertStylesAt = html.lastIndexOf("</body>");
     return `
       <!DOCTYPE html>
       ${html.slice(0, insertStylesAt)}
       ${sheet.getStyleTags()}
       ${html.slice(insertStylesAt)}
-    `.trim()
-  },
-}
+    `.trim();
+  }
+};
 
-module.exports = siteConfig
+module.exports = siteConfig;
